faed4740fac17fc91a8e55a93cdea54712581062
plumplum01 <plumplum010101@gmail.com>
2024-08-27 20:18:33 +0900

    이미지 적용 완료

--- src/pages/walky.jsx
+++ src/pages/walky.jsx
@@ -1,10 +1,13 @@
 import './walky.css'
+import focusImage from './focus.svg'; // 이미지 파일을 같은 폴더에서 import
 
 const WalkyPage = () => {
     return (
         <div className="container">
-            <div className="svg"><img src="/focus.svg" alt="" /></div>
-            {/* 이미지 어케 불러오는지 몰겠다!! */}
+            <div className="focusWrap">
+                <div className="focusSvg"><img src={focusImage} alt="Focus" /></div>
+                <div className="focusSvg"><img src={focusImage} alt="Focus" /></div>
+            </div>
             <div className="walkySub">Where                     are        you                 now?</div>
             <div className="walkyTitle">
                 Walky
