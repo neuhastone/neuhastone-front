7275d58d52258f1f8c4885460dd8c56a3f60864a
plumplum01 <plumplum010101@gmail.com>
2024-08-28 23:38:29 +0900

    gsap Text 적용

--- src/pages/walky.jsx
+++ src/pages/walky.jsx
@@ -3,13 +3,24 @@ import './walky.css';
 import focusImage from './focus.svg'; // 이미지 파일을 같은 폴더에서 import
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { TextPlugin } from "gsap/TextPlugin";
 
 const WalkyPage = () => {
-    useEffect(() => {
-        // 컴포넌트가 마운트될 때 GSAP 애니메이션을 적용
-        gsap.to(".callBtn", { rotation: 360, x: 100, duration: 3 });
-    }, []); // 빈 배열은 의존성 배열로, 이 효과가 컴포넌트가 처음 마운트될 때 한 번만 실행됨.
+    
+    gsap.registerPlugin(TextPlugin);
 
+    useGSAP(()=>{
+        gsap.to(".callBtn", {
+            rotation: 360, x: 100, duration: 1 
+        });
+
+        gsap.to(".text", {
+            duration: 2,
+            text: "안녕하세요 삥삥이에요",
+            delay: 1
+        });
+    },[]);
+    
     return (
         <div className="container">
             <div className="focusWrap">
@@ -26,8 +37,21 @@ const WalkyPage = () => {
                 <div><img className='focusSvg' src={focusImage} alt="Focus" /></div>
                 <div><img className='focusSvg' src={focusImage} alt="Focus" /></div>
             </div>
-            <div className="callBtn">
-                <p>start</p>
+            <div className="btnContainer">
+                <div className="callBtn">
+                    <p>start</p>
+                </div>
+            </div>
+            <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Reprehenderit aut sunt expedita! Tempora illo corrupti inventore 
+                fugit doloribus? Alias, porro. Lorem, ipsum dolor sit amet 
+                consectetur adipisicing elit. Facilis recusandae reiciendis 
+                pariatur autem corporis labore sed architecto unde nostrum 
+                voluptas eum sequi repellat voluptatem fuga, accusamus dicta 
+                possimus magni fugit obcaecati debitis tempora consequatur 
+                tenetur. Officiis eaque possimus illum esse deserunt mollitia 
+                distinctio enim eius quod minima totam, nisi architecto?
             </div>
         </div>
     );
