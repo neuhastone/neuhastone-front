41b5110fdb5a9ba6024fba67b2d681880fd29b8a
plumplum01 <plumplum010101@gmail.com>
2024-08-28 21:57:02 +0900

    gsap 적용용

--- src/pages/walky.jsx
+++ src/pages/walky.jsx
@@ -1,17 +1,15 @@
-import './walky.css'
+import React, { useEffect } from 'react';
+import './walky.css';
 import focusImage from './focus.svg'; // 이미지 파일을 같은 폴더에서 import
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 
-
-// gsap.registerPlugin(useGSAP);
-gsap.to(".callBtn", { // 타겟
-    x: 200, // 속성
-    duration: 2// 속성 값(선택)
-    });
-
-
 const WalkyPage = () => {
+    useEffect(() => {
+        // 컴포넌트가 마운트될 때 GSAP 애니메이션을 적용
+        gsap.to(".callBtn", { rotation: 360, x: 100, duration: 3 });
+    }, []); // 빈 배열은 의존성 배열로, 이 효과가 컴포넌트가 처음 마운트될 때 한 번만 실행됨.
+
     return (
         <div className="container">
             <div className="focusWrap">
