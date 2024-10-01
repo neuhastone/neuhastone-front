c8a06a2d217d8d682f4c2c9200f3d824eeaae2f2
jeong <jw.jeong@tilda.co.kr>
2024-10-01 16:30:59 +0900

    로그인 기능

--- src/widgets/user/api/login.js
+++ src/widgets/user/api/login.js
@@ -1,8 +1,10 @@
 import axiosInstance from '@shared/api/axios';
 
 
-const requestLogin = async () => {
-  return await axiosInstance.post('/login'); // API 엔드포인트 호출
+const requestLogin = async (body) => {
+  const res = await axiosInstance.post('/token',body); // API 엔드포인트 호출
+
+  return {status: res.status, data:res.data}
 }
 
 
