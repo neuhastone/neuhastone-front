0ac862e71a215331cc5164cb2efa86507da5ea51
jeong <jw.jeong@tilda.co.kr>
2024-10-01 16:31:40 +0900

    프로젝트 조회

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,8 +1,20 @@
 import ProjectForm from '@widgets/project/ui/projectForm';
+import {getCookie} from  '@shared/cookie/cookie'
+import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const ProjectPage = () => {
-  useEffect(()=>{console.log("test")},[])
+  const accessToken = getCookie("accessToken");
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    console.log(accessToken)
+    if (accessToken===undefined){
+      navigate("/")
+    }
+  },[])
+
   return (
     <ProjectForm />  
   );
--- src/widgets/project/ui/projectForm.jsx
+++ src/widgets/project/ui/projectForm.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 
-
 import CardList from '@components/project/ui/cardList'
 import getProjects from '@widgets/project/api/getProjects';
 
 import '@widgets/project/ui/project.css'
+import { useNavigate } from 'react-router-dom';
 
 const ProjectForm = () => {
   const [project, setProject] = useState()
   const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/create-project');
+  };
 
   useEffect(()=>{
     const fetchData = async () => {
@@ -24,10 +29,10 @@ const ProjectForm = () => {
 
   return(
     
-    <div className="app">
+    <div className="project-form">
       <h1>프로젝트 둘러보기</h1>
       {isLoading ? <>로딩중</> :<CardList cards = {project.projects}/>}
-      <button className="create-button">프로젝트 만들기</button>
+      <button className="create-button" onClick={()=> handleClick()}>프로젝트 만들기</button>
     </div>
   )
 }
